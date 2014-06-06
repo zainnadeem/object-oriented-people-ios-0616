620d76d3018d1dfac514fd4604569b88f5843035
altyus <alctyus@gmail.com>
2014-06-06 09:38:54 -0400

    updated gitignore to include cocoapods

---
+++ ObjectOrientedPeople/ObjectOrientedPeople/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjectOrientedPeople
+//
+//  Created by Al Tyus on 6/6/14.
+//  Copyright (c) 2014 al-tyus.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectOrientedPeople/ObjectOrientedPeople/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjectOrientedPeople
+//
+//  Created by Al Tyus on 6/6/14.
+//  Copyright (c) 2014 al-tyus.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
