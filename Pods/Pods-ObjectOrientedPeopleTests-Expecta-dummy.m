f4cbe22526381514b2508a536439a82127141fdb
markedwardmurray <markedwardmurray@gmail.com>
2015-09-23 15:52:55 -0400

    pods: specta 1.0.4 expecta 1.0.3, deployment ios 8.0

--- Pods/Pods-ObjectOrientedPeopleTests-Expecta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ObjectOrientedPeopleTests_Expecta : NSObject
-@end
-@implementation PodsDummy_Pods_ObjectOrientedPeopleTests_Expecta
-@end
